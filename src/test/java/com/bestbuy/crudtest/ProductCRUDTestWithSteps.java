e8c676e83f449c832c21810cbf2147c75d4fc516
Shraddhadhaduk <shraddhadhaduk96@gmail.com>
2024-02-09 00:51:44 +0000

    serenity bdd 01

---
+++ src/test/java/com/bestbuy/crudtest/ProductCRUDTestWithSteps.java
@@ -0,0 +1,11 @@
+package com.bestbuy.crudtest;
+
+import com.bestbuy.testbase.TestBase;
+import net.serenitybdd.junit.runners.SerenityRunner;
+import org.junit.runner.RunWith;
+
+@RunWith(SerenityRunner.class)
+public class ProductCRUDTestWithSteps extends TestBase {
+
+
+}
